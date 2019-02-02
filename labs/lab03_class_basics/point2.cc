570fde5ffa6c98ec3d53ff1d2cd82af33bc95b9d
Angela Almquist <almqu071@umn.edu>
2019-02-02 17:06:53 -0600

    add(lab03): Adding lab03 deliverables

---
+++ labs/lab03_class_basics/point2.cc
@@ -0,0 +1,56 @@
+#include <cmath>
+#include <iostream>
+
+#include "point2.h"
+Point2::Point2( float x, float y ) {
+  position_[0] = x;
+  position_[1] = y;
+}
+float Point2::DistanceBetween( Point2 cluster){
+	//pythagrean thm
+	float distance = sqrt(pow((position_[0] - cluster.position_[0]), 2.0) 
+	+ pow((position_[1] - cluster.position_[1]), 2.0));
+	
+	return distance; 
+}
+
+void Point2::Print() {
+  std::cout
+  << "[" << position_[0] << ", " << position_[1] << "]"<< std::endl;
+}
+
+int Point2::Quadrant() {
+	if (position_[0] == 0 && position_[1] == 0)
+	{	
+		return 0; } //origin
+	else if (position_[0] >= 0)
+	{
+		if (position_[0] == 0) //borders
+		{
+			if (position_[1] > 0)
+			{ 
+				return 2; } 
+			else
+			{ 
+				return 4;}
+		}
+		else if (position_[1] >= 0)
+		{
+			return 1; } //border and common case
+		else 
+		{ 
+			return 4; }
+	}
+	else
+	{
+		if (position_[1] == 0) //border
+		{	
+			return 3; }
+		else if (position_[1] > 0)
+		{
+			return 2; }
+		else
+		{
+			return 3; }
+	}
+}
---
+++ labs/lab03_class_basics/point2.h
@@ -0,0 +1,14 @@
+#ifndef LAB03_POINT2_CLASSES_H_
+#define LAB03_POINT2_CLASSES_H_
+
+class Point2 {
+  public:
+    Point2(float x = 0.0, float y = 0.0); //default sets to zero if no input
+	float DistanceBetween( Point2 cluster);
+	int Quadrant();
+	void Print();
+	
+  private:
+    float position_[2];
+  };
+#endif /* LAB03_POINT2_CLASSES_H_ */
